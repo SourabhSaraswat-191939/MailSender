d46478d9f8f6d022c03a0c65dcc846328259bbbf
Sourabh Saraswat <60272417+SourabhSaraswat@users.noreply.github.com>
2020-10-19 09:17:53 +0530

    Update views.py

--- MailSender/SendEmail/views.py
+++ MailSender/SendEmail/views.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 
-# Create your views 
+# Create your views here.
 
 def home(request):
     return render(request, 'index.html')
